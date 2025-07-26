7ae385e0acf3b71d275e6fec0f8fbb56f1b49ba7
aldenfachetti <aldenmerlin@gmail.com>
2025-07-27 01:41:19 +0200

    chore: remove unused CookieConsent component after refactor to ConsentBanner

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,16 @@
 import React from "react";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
-import CookieConsent from "./components/CookieConsent";
 import { ConsentBanner } from "./components/ConsentBanner";
 import "./i18n";
 
 const App: React.FC = () => {
   return (
-    <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
-      <ConsentBanner />
+    <div className="min-h-screen bg-gray-900 text-white flex flex-col relative">
       <Layout>
         <Home />
       </Layout>
-      <CookieConsent />
+      <ConsentBanner />
     </div>
   );
 };
